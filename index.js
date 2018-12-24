ab4928e4eee059564a8fdedd10c0c78a878a0ca1
Daniel Glover <iamdanielglover@gmail.com>
2018-12-24 19:48:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function dwarfRollCall(dwarves) {
 function dwarfRollCall(dwarves) {
    var array = [];
    
-   for(var i = 0; i < dwarves.length; i++){
+   for(var i = 0; i < dwarves.length/2; i++){
      
    }
    do {
