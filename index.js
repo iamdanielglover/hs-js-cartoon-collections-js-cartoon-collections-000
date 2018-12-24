b69582143716667d1ac4df45f5e0c9d7004113ce
Daniel Glover <iamdanielglover@gmail.com>
2018-12-24 19:41:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -40,3 +40,18 @@ function findTheCheese(foods) {
 }
 
 //It iterates through each words in the 'foods' array. The if statement returns a value - if the current iterated value in the 'foods' array is either 'cheddar, camembert or gouda' then it will return that current value (marked as i) in question. If none of the conditions in the if statement are met, return 'no cheese!'. Simple!
+
+
+
+function startsWithB(words){
+  var barray = [];
+  for (var i = 0; i < words.length; i++) {
+    if (words[i] ===)
+  }
+}
+
+"brian".startsWith("b")
+
+startsWithB(['brian', 'jill', 'jane']) // ['brian']
+startsWithB(['banana', 'berry', 'cherry']) // ['banana', 'berry']
+startsWithB(['dwarf', 'planeteer', 'cheese']) // []
